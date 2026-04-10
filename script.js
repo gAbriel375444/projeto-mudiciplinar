5a0f26d0c9c593f71fbe6e6c2e37dbb7872c5f88
Gabriel Silva <gaabrielsilva375@gmail.com>
2026-04-10 11:42:26 -0300

    Começo da parte funcional
    index.html:
    
    Ajeitei a estrutura do html;
    
    Ajeitei o id do texto;
    
    Comentei a parte de mensagens do usuario/bot;
    
    Criei um botão de enviar mensagens
    
    Criei um <p> para exibir a resposta do "bot";
    
    Linkei o js com o <script>
    
    script.js:
    
    Criei a var respostas que é um dicionario, contendo as possiveis mensagens do usuario e as respostas para elas;
    
    Criei uma var para o botão de enviar mensagens e peguei o id lá do index;
    
    Adicionei um evento ao clicar no botão:
    
    Criei uma var que pega o id do meu texto;
    
    Criei outra para pegar o id do <p>;
    
    então eu fiz
    
    SE a mensagem do usuario for valida
    (estiver dentro do dicionario)
    
    retorne a resposta correspondete
    e exiba atraves do <p>
    
    SENÃO
    
    Retorne "Não entendi a pergunta"
    atraves do <p>

---
+++ script.js
@@ -0,0 +1,37 @@
+// dicionário de respostas
+const respostas = {
+    "oi": "Oi, tudo bem com você?",
+    "olá": "Olá, tudo bem com você?",
+    "tudo bem": "Que bom! Quer conversar sobre algo?"
+};
+
+//botão para enviar a mensagem
+let botao_enviar_mensagem = document.getElementById("enviar_mensagem");
+
+//ao clicar no botão
+botao_enviar_mensagem.addEventListener("click", function () {
+    
+    //pego id do meu texto e salvo numa var
+    const input = document.getElementById("meu_texto").value.toLowerCase();
+
+    //no meu index, criei um p para aparecer a resposta na tela
+    //aqui estou pegando o id desse <p> 
+    const respostaElemento = document.getElementById("resposta");
+
+    //se a respota digitada for valida (está dentro do dicionario)
+    if (respostas[input])
+    {
+        
+        //ele escreve a resposta correspondente 
+        //e faz aparecer na tela atraves do <p>
+        respostaElemento.innerText = respostas[input];
+
+    } 
+    
+    else //se não
+    {
+
+        //ele retorna isso
+        respostaElemento.innerText = "Não entendi a pergunta.";
+    }
+});
